09964628799690f086a3e463db817b27fea13dbc
DanielFH <danfh9@gmail.com>
2024-11-16 10:55:17 +0200

    changed name

--- src/main/java/il/ac/tau/cs/hanukcoin/Validate.java
+++ src/main/java/il/ac/tau/cs/hanukcoin/Validate.java
@@ -4,7 +4,7 @@ import java.io.InputStream;
 
 public class Validate {
 
-    public static Block validate(InputStream stream) {
+    public static Block validateBlock(InputStream stream) {
         // TODO
     }
 }
